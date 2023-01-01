d50173bfe34032b38e29fba1e554c4d0d06de644
Dennet2022 <120566019+Dennet2022@users.noreply.github.com>
2023-01-01 18:55:52 +0500

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,7 +4,7 @@
 
 
             <div class=" long">
-                <a class="navbar-brand footer-brand" href="/">Kartainvestora</a>
+                <a class="navbar-brand footer-brand" href="/">Kartainvestora1</a>
             </div>
             <div class="icons">
 
