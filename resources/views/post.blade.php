38f335febf212d743e209aab4c8486132794f8e9
Serhii V <serhii.voronovych@gmail.com>
2023-01-05 18:42:58 +0200

    UPD

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -19,7 +19,7 @@
 
         @if(isset($post->graph))
             <div class="row">
-                <div class="col-md-12" style="margin: 20px 0;">
+                <div class="col-md-12" style="margin: 0 3.6% 20px 3.6%;">
                     {!! $post->graph !!}
                 </div>
             </div>
