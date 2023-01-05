d4a6b3881e7d3577c080562380672c150207f1c2
Serhii V <serhii.voronovych@gmail.com>
2023-01-05 18:35:43 +0200

    UPD

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,7 +45,7 @@
             <hr>
 
             <div class="card  row">
-                <strong style="float:right; margin-right: 25px; font-size:15px;">{{ \Carbon\Carbon::parse($post->created_at)->toDateString() }}</strong>
+                <strong style="float:right; padding:15px; color:grey; display: block; font-size:15px;">{{ \Carbon\Carbon::parse($post->created_at)->toDateString() }}</strong>
                 <h5 class="card-title">
                     {{ $post->title }}
                 </h5>
