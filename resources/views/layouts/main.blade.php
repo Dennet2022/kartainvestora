cb2c875b2cba00935033d4f8803c42d828d1c031
Serhii V <serhii.voronovych@gmail.com>
2023-01-10 12:49:40 +0200

    UPD

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -221,11 +221,11 @@
         $(window).resize(function() {
             $( "iframe" ).attr('width', $(window).width()).attr('height', $(window).height()/100*80);
             console.log('window width');
-            console.log($(window).width()/100*95);
+            console.log($(window).width()/100*80);
         });
-        $( "iframe" ).attr('width', $(window).width()/100*95).attr('height', $(window).height()/100*80);
+        $( "iframe" ).attr('width', $(window).width()/100*80).attr('height', $(window).height()/100*80);
         console.log('window width');
-        console.log($(window).width()/100*95);
+        console.log($(window).width()/100*80);
     });
 </script>
 
