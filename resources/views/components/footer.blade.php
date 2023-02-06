423ecc84aaf9c2d8d40cf44aca0befd24cd8914a
Dennet2022 <120566019+Dennet2022@users.noreply.github.com>
2023-02-06 22:14:37 +0500

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -19,14 +19,14 @@
         </div>
         <div class="sec sec2">
 
-            <a href="http://kartainvestora.ru/" class="fit">
+           <!-- <a href="http://kartainvestora.ru/" class="fit">
                 <div class="donate button">
                     Поддержать
                 </div>
             </a>
             <p class="bit">
                 <font class="black" style="font-size:18px;">Bitcoin Donations: </font> <br>1N3gGn5WrFDRqjBPKgxHjg9crcMUUuayAk
-            </p>
+            </p> -->
 
         </div>
         <div class="sec sec3">
@@ -45,12 +45,12 @@
         <div class="long-resp">
             <a class="navbar-brand" href="/">Kartainvestora</a>
         </div>
-        <a href="http://kartainvestora.ru/" class="fit">
+       <!-- <a href="http://kartainvestora.ru/" class="fit">
             <div class="donate-resp button">
                 Поддержать
             </div>
         </a>
-        <p class="bit-resp">Bitcoin Donations: 1N3gGn5WrFDRqjBPKgxHjg9crcMUUuayAk</p>
+        <p class="bit-resp">Bitcoin Donations: 1N3gGn5WrFDRqjBPKgxHjg9crcMUUuayAk</p> -->
         <a href="http://kartainvestora.ru/" class="news-resp footer-log">Новости</a>
         <a href="http://kartainvestora.ru/" class="terms footer-log">О Нас</a>
         <a href="http://kartainvestora.ru/" class="footer-log" target="_blank">Рассылка</a>
